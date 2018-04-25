9c8dd1d860930f642d22e74bfb532ca6be063efc
alecfn <apfor2@student.monash.edu>
2018-04-25 21:28:58 +1000

    Working on the map fragment for the ui. Bit weird in Kotlin

--- PhotomapProject/app/src/main/java/com/alecforbes/photomaps/CustomPhotomap.kt
+++ PhotomapProject/app/src/main/java/com/alecforbes/photomaps/CustomPhotomap.kt
@@ -1,15 +1,28 @@
 package com.alecforbes.photomaps
 
 import android.os.Bundle
+import android.support.v4.app.FragmentActivity
 import android.support.v7.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapFragment
+import com.google.android.gms.maps.OnMapReadyCallback
 
-class CustomPhotomap : AppCompatActivity() {
+class CustomPhotomap : FragmentActivity(), OnMapReadyCallback {
+
+    override fun onMapReady(p0: GoogleMap?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val imagesIntent = intent
         setContentView(R.layout.acivity_photomap)
+        
+        val customMapFragment = supportFragmentManager.findFragmentById(R.id.photomapFragment) as MapFragment
+        customMapFragment.getMapAsync(customMapFragment)
+
+
 
 
     }
