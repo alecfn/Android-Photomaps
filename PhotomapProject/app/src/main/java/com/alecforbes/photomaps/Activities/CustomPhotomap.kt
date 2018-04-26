d588231f2f29c285e318a319fbe8f7e28718b478
alecfn <apfor2@student.monash.edu>
2018-04-26 20:09:47 +1000

    Got the uris to pass properly, gad to getParcelableExtra and cast to Intent

--- PhotomapProject/app/src/main/java/com/alecforbes/photomaps/Activities/CustomPhotomap.kt
+++ PhotomapProject/app/src/main/java/com/alecforbes/photomaps/Activities/CustomPhotomap.kt
@@ -3,7 +3,9 @@ package com.alecforbes.photomaps.Activities
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.os.Parcelable
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import com.alecforbes.photomaps.R
 
 class CustomPhotomap : AppCompatActivity() {
@@ -28,8 +30,22 @@ class CustomPhotomap : AppCompatActivity() {
 
         if (imagesIntent != null) {
 
+            var imageData = imagesIntent.getParcelableExtra<Intent>("imageData")
+            //imageData.
+
+            var numberImages = imageData.clipData!!.itemCount
+            var imageUris = ArrayList<String>()
+
+            // Get all of the image ClipData objects to add to an array and send in an intent
+            for (i in 0..(numberImages - 1)){
+                var uri = imageData.clipData.getItemAt(i).uri.toString()
+                imageUris.add(uri)
+            }
+
+
+
             //val selectedImageUris = imagesIntent.getSerializableExtra("imageData")
-            //print("bleh")
+            print("bleh")
         }
     }
 
