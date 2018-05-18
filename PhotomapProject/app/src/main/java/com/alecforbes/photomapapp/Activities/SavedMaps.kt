c808ce7d88bc7177061d98b2c408aee37f4c8993
Alec Forbes-Nicholson <alec@teratext.leidos.com.au>
2018-05-18 14:18:44 +1000

    Refactored the saved maps list to just be a basic list view because of the URI access issues.

--- PhotomapProject/app/src/main/java/com/alecforbes/photomapapp/Activities/SavedMaps.kt
+++ PhotomapProject/app/src/main/java/com/alecforbes/photomapapp/Activities/SavedMaps.kt
@@ -7,13 +7,12 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.support.v7.widget.CardView
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.widget.*
+import com.alecforbes.photomapapp.Activities.Photomaps.PlacePhotomap
 import com.alecforbes.photomapapp.Controllers.Database.DatabaseHelper
 import com.alecforbes.photomapapp.Model.ImageData
 import com.alecforbes.photomapapp.R
+import kotlinx.android.synthetic.main.activity_places_list.*
 import kotlinx.android.synthetic.main.activity_saved_maps.*
 import java.io.InputStream
 
@@ -62,17 +61,35 @@ class SavedMaps : AppCompatActivity() {
             val savedPreview = ImageView(this)
             val viewButton = Button(this, null, R.style.PrimaryFlatButton)
 
-            savedName.text = it.key
-            viewButton.text = getString(R.string.view)
+            //savedName.text = it.key
+            //viewButton.text = getString(R.string.view)
 
-            savedCardLinearLayout.addView(savedName)
-            savedCardLinearLayout.addView(savedPreview)
-            savedCardLinearLayout.addView(viewButton)
+            //savedCardLinearLayout.addView(savedName)
+            //savedCardLinearLayout.addView(savedPreview)
+            //savedCardLinearLayout.addView(viewButton)
 
             // Now create a bitmap from the first uri to act as the image preview for the saved map
 
             // Only system apps can use MANAGE_DOCUMENTS permissions, so get the byte stream
+            //val absPath = it.value[0].path
             //val previewUriStream = contentResolver.openInputStream(it.value[0])
+
+            try{
+
+                val savedMapAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, ArrayList(savedMaps.keys))
+                savedMapListView.adapter = savedMapAdapter
+
+                placesListView.onItemClickListener = AdapterView.OnItemClickListener { adapterView, view, i, l ->
+
+                    //val selectedLoc = placesListView.getItemAtPosition(i) as String
+                    //val placePhotoMapIntent = Intent(this, PlacePhotomap::class.java)
+                    //placePhotoMapIntent.putExtra("SelectedLocation", selectedLoc)
+                    //startActivity(placePhotoMapIntent)
+                }
+
+            }catch (ex: Exception){
+                // todo put in a placeholder or something
+            }
 
             //val byteData = previewUriStream.readBytes()
 
@@ -81,6 +98,7 @@ class SavedMaps : AppCompatActivity() {
             //val previewBitmap = BitmapFactory.decodeByteArray(byteData, 0, byteData.size)
 
             //savedPreview.setImageBitmap(previewBitmap)
+
 
 
         }
