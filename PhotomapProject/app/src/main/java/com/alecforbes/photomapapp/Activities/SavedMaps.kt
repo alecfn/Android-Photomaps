05b7a3ff41c8cc2dfef3709e00c07f0073138ea2
Alec Forbes-Nicholson <alec@teratext.leidos.com.au>
2018-05-18 12:20:01 +1000

    Set up population of card view with saved maps basics

--- PhotomapProject/app/src/main/java/com/alecforbes/photomapapp/Activities/SavedMaps.kt
+++ PhotomapProject/app/src/main/java/com/alecforbes/photomapapp/Activities/SavedMaps.kt
@@ -1,10 +1,21 @@
 package com.alecforbes.photomapapp.Activities
 
+import android.content.Intent
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v7.widget.CardView
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import com.alecforbes.photomapapp.Controllers.Database.DatabaseHelper
+import com.alecforbes.photomapapp.Model.ImageData
 import com.alecforbes.photomapapp.R
+import kotlinx.android.synthetic.main.activity_saved_maps.*
+import java.io.InputStream
 
 class SavedMaps : AppCompatActivity() {
 
@@ -32,17 +43,50 @@ class SavedMaps : AppCompatActivity() {
 
         savedMapNames.forEach {
             val savedMapUris = databaseHelper.getSavedMapUris(it)
-            savedMaps.put(it,savedMapUris)
-            print("")
+            savedMaps[it] = savedMapUris
         }
-        //val test = databaseHelper.getSavedMapUris(savedMaps[0])
-        //print("")
+
     }
 
     /**
      * Get all the map names saved in the database and the first image to use as a preview
      */
     private fun populateSavedView(){
+
+        // Duplicate the card layout and populate with the retrieved information
+
+        savedMaps.forEach {
+
+
+            val savedName = TextView(this)
+            val savedPreview = ImageView(this)
+            val viewButton = Button(this)
+
+            savedName.text = it.key
+            viewButton.text = getString(R.string.view)
+
+            savedCardLinearLayout.addView(savedName)
+            savedCardLinearLayout.addView(savedPreview)
+            savedCardLinearLayout.addView(viewButton)
+
+            // Now create a bitmap from the first uri to act as the image preview for the saved map
+
+            // Only system apps can use MANAGE_DOCUMENTS permissions, so get the byte stream
+            val previewUriStream = contentResolver.openInputStream(it.value[0])
+
+            val byteData = previewUriStream.readBytes()
+
+            previewUriStream.close()
+
+            val previewBitmap = BitmapFactory.decodeByteArray(byteData, 0, byteData.size)
+
+            savedPreview.setImageBitmap(previewBitmap)
+
+
+        }
+
+
+
 
     }
 
