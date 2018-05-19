7599b527d913a0d30dba4ddc0506448ee958c791
alecfn <apfor2@student.monash.edu>
2018-05-19 21:05:19 +1000

    Button now opens maps, not sure if its connecting properly though

--- PhotomapProject/app/src/main/java/com/alecforbes/photomapapp/Activities/Photomaps/CustomPhotomap.kt
+++ PhotomapProject/app/src/main/java/com/alecforbes/photomapapp/Activities/Photomaps/CustomPhotomap.kt
@@ -5,6 +5,7 @@ package com.alecforbes.photomapapp.Activities.Photomaps
 import android.content.Intent
 import android.location.Location
 import android.location.LocationManager
+import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AlertDialog
@@ -267,6 +268,15 @@ open class CustomPhotomap : AppCompatActivity(), OneMoreFabMenu.OptionsClick {
             imageTimeTakenValue.text = splitDateTime[1]
         }else{
             imageTimeTakenValue.text = "Unknown"
+        }
+
+        // Set the view in maps button to take the user to the Google Maps app from the listed addr
+        viewInMapsButton.setOnClickListener {
+            val mapsIntent = Intent(android.content.Intent.ACTION_VIEW, Uri.parse(imageData.realAddress))
+            mapsIntent.setClassName("com.google.android.apps.maps",
+                    "com.google.android.maps.MapsActivity")
+
+            startActivity(mapsIntent)
         }
 
         photomapIndvImageView.visibility = View.VISIBLE
