b16a2f0e5e00294c45d96bac08900dd979ce7d73
alecfn <apfor2@student.monash.edu>
2018-05-28 21:52:13 +1000

    Deleted a bunch of seemingly redundant code that was meant to to get and image preview, could maybe get that still now that saving works.

--- PhotomapProject/app/src/main/java/com/alecforbes/photomapapp/Activities/SavedMaps.kt
+++ PhotomapProject/app/src/main/java/com/alecforbes/photomapapp/Activities/SavedMaps.kt
@@ -47,28 +47,20 @@ class SavedMaps : AppCompatActivity() {
      */
     private fun populateSavedView(){
 
-
         savedMaps.forEach {
-
-
-            // Now create a bitmap from the first uri to act as the image preview for the saved map
-
-            // Only system apps can use MANAGE_DOCUMENTS permissions, so get the byte stream
-            //val absPath = it.value[0].path
-            //val previewUriStream = contentResolver.openInputStream(it.value[0])
 
             try{
 
                 val savedMapAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, ArrayList(savedMaps.keys))
                 savedMapListView.adapter = savedMapAdapter
 
-                savedMapListView.onItemClickListener = AdapterView.OnItemClickListener { adapterView, view, i, l ->
+                savedMapListView.onItemClickListener = AdapterView.OnItemClickListener {
+                    adapterView, view, i, l ->
 
                     // todo make this look pretty, maybe colour and different layout
                     val selectedSavedMap = savedMapListView.getItemAtPosition(i) as String
                     val savedMapIntent = Intent(this, CustomPhotomap::class.java)
                     savedMapIntent.putExtra("SavedMapName", selectedSavedMap)
-                    //savedMapIntent.putExtra("SelectedMapImages", savedMaps[selectedSavedMap])
                     val selectedMapImages = savedMaps[selectedSavedMap]
 
                     // Data returned from the DB will be of 'URIString' data types, recast to string
@@ -80,20 +72,23 @@ class SavedMaps : AppCompatActivity() {
                     // Now create the image data objects fixme dont do this here cant be parceled
                     savedMapIntent.putStringArrayListExtra("SavedImageUris", selectedImageUriStrings)
                     savedMapIntent.putExtra("IsSavedMap", true)
-                    //val savedFileData = FileDataController(contentResolver, screenSize = 0)  // todo screen size here?
-                    //savedFileData.getSelectedImageUrisFromArray(selectedMapImages!!)
-                    //savedMapIntent.putExtra("SavedMapImages", savedFileData.selectedData)
-
-                    //val selectedImages = ArrayList<ImageData>()
-
 
                     startActivity(savedMapIntent)
 
                 }
+                // Long click listener to delete saved maps
+
+                savedMapListView.onItemLongClickListener = AdapterView.OnItemLongClickListener {
+                    adapterView, view, i, l ->
+
+                }
+
 
             }catch (ex: Exception){
                 // todo put in a placeholder or something
             }
+
+
 
             //val byteData = previewUriStream.readBytes()
 
