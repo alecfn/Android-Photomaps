a951e47ca9efa20bfc58422f8a73503ac9d64343
alecfn <apfor2@student.monash.edu>
2018-06-03 16:05:10 +1000

    New wikipedia links for each item in places.

--- PhotomapProject/app/src/main/java/com/alecforbes/photomapapp/Model/PlacesLinksHashmap.kt
+++ PhotomapProject/app/src/main/java/com/alecforbes/photomapapp/Model/PlacesLinksHashmap.kt
@@ -11,37 +11,38 @@ package com.alecforbes.photomapapp.Model
 
 class PlacesLinksHashmap{
 
-    private val placesLinksHashmap = HashMap<String, List<String>>()
+    private val placesLinksHashmap = HashMap<String, List<List<String>>>()
 
     init {
-        val sydneyLinks = listOf("PlacesTestData/Sydney/harbourbridge.jpg",
-                "PlacesTestData/Sydney/operahouse.jpg",
-                "PlacesTestData/Sydney/parliament.jpeg")
+        val sydneyLinks =
+                listOf(listOf("PlacesTestData/Sydney/harbourbridge.jpg", "https://en.wikipedia.org/wiki/Sydney_Harbour_Bridge"),
+                listOf("PlacesTestData/Sydney/operahouse.jpg", "https://en.wikipedia.org/wiki/Sydney_Opera_House"),
+                        listOf("PlacesTestData/Sydney/parliament.jpeg", "https://en.wikipedia.org/wiki/Parliament_House,_Sydney"))
 
-        val newYorkLinks = listOf("PlacesTestData/NewYork/brooklynbridge.jpg",
-                "PlacesTestData/NewYork/empirestate.jpg",
-                "PlacesTestData/NewYork/statueofliberty.jpg")
+        val newYorkLinks = listOf(listOf("PlacesTestData/NewYork/brooklynbridge.jpg", "https://en.wikipedia.org/wiki/Brooklyn_Bridge"),
+                listOf("PlacesTestData/NewYork/empirestate.jpg", "https://en.wikipedia.org/wiki/Empire_State_Building"),
+                listOf("PlacesTestData/NewYork/statueofliberty.jpg", "https://en.wikipedia.org/wiki/Statue_of_Liberty"))
 
-        val londonLinks = listOf("PlacesTestData/London/londoneye.jpg",
-                "PlacesTestData/London/parliament.jpg",
-                "PlacesTestData/London/toweroflondon.jpg")
+        val londonLinks = listOf(listOf("PlacesTestData/London/londoneye.jpg", "https://en.wikipedia.org/wiki/London_Eye"),
+                listOf("PlacesTestData/London/parliament.jpg", "https://en.wikipedia.org/wiki/Palace_of_Westminster"),
+                listOf("PlacesTestData/London/toweroflondon.jpg", "https://en.wikipedia.org/wiki/Tower_of_London"))
 
-        val worldLinks = listOf("PlacesTestData/World/brandenburggate.jpg",
-                "PlacesTestData/World/eiffeltower.jpg",
-                "PlacesTestData/World/goldengate.jpg",
-                "PlacesTestData/World/hollywoodsign.jpg",
-                "PlacesTestData/World/liberty.jpg",
-                "PlacesTestData/World/operahouse.jpg",
-                "PlacesTestData/World/pyramids.jpg",
-                "PlacesTestData/World/rushmore.jpg",
-                "PlacesTestData/World/touhontower.jpg")
+        val worldLinks = listOf(listOf("PlacesTestData/World/brandenburggate.jpg", "https://en.wikipedia.org/wiki/Brandenburg_Gate"),
+                listOf("PlacesTestData/World/eiffeltower.jpg", "https://en.wikipedia.org/wiki/Eiffel_Tower"),
+                listOf("PlacesTestData/World/goldengate.jpg", "https://en.wikipedia.org/wiki/Golden_Gate_Bridge"),
+                listOf("PlacesTestData/World/hollywoodsign.jpg", "https://en.wikipedia.org/wiki/Hollywood_Sign"),
+                listOf("PlacesTestData/World/liberty.jpg", "https://en.wikipedia.org/wiki/Statue_of_Liberty"),
+                listOf("PlacesTestData/World/operahouse.jpg", "https://en.wikipedia.org/wiki/Sydney_Opera_House"),
+                listOf("PlacesTestData/World/pyramids.jpg", "https://en.wikipedia.org/wiki/Giza_pyramid_complex"),
+                listOf("PlacesTestData/World/rushmore.jpg", "https://en.wikipedia.org/wiki/Mount_Rushmore"),
+                listOf("PlacesTestData/World/touhontower.jpg", "https://en.wikipedia.org/wiki/Tokyo_Tower"))
 
-        val ausBigThingsLinks = listOf("PlacesTestData/AusBigThings/bigbanana.jpg," +
-                "PlacesTestData/AusBigThings/bigkoala.jpg",
-                "PlacesTestData/AusBigThings/biglobster.jpg",
-                "PlacesTestData/AusBigThings/bigpineapple.jpg",
-                "PlacesTestData/AusBigThings/bigram.jpg",
-                "PlacesTestData/AusBigThings/bigstrawberry.jpg")
+        val ausBigThingsLinks = listOf(listOf("PlacesTestData/AusBigThings/bigbanana.jpg", "https://en.wikipedia.org/wiki/Big_Banana"),
+                listOf("PlacesTestData/AusBigThings/bigkoala.jpg", "https://en.wikipedia.org/wiki/Australia%27s_big_things"),
+                listOf("PlacesTestData/AusBigThings/biglobster.jpg", "https://en.wikipedia.org/wiki/Big_Lobster"),
+                listOf("PlacesTestData/AusBigThings/bigpineapple.jpg", "https://en.wikipedia.org/wiki/Big_Pineapple"),
+                listOf("PlacesTestData/AusBigThings/bigram.jpg", "https://en.wikipedia.org/wiki/Big_Merino"),
+                listOf("PlacesTestData/AusBigThings/bigstrawberry.jpg", "https://en.wikipedia.org/wiki/Australia%27s_big_things"))
 
         placesLinksHashmap["Sydney"] = sydneyLinks
         placesLinksHashmap["New York"] = newYorkLinks
@@ -51,7 +52,7 @@ class PlacesLinksHashmap{
 
     }
 
-    fun getPlaceLinks(key: String): List<String>? {
+    fun getPlaceLinks(key: String): List<List<String>>? {
         return placesLinksHashmap[key]
     }
 }
